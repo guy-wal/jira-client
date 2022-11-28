7b3226ea2e7e7d3c0217c8ae4ff53b0f24c14dbd
guy-wal <guyw@veriti.ai>
2022-11-28 12:53:55 +0200

    add generate-client script and readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# jira-client
-jira client from openapi
+# openapi jira-client
+
+## Client Generation Instructions
+
+1. `npmrc` file must include:
+
+```
+@wised:registry=https://wised.jfrog.io/artifactory/api/npm/wised-npm-repo/
+//wised.jfrog.io/artifactory/api/npm/wised-npm-repo/:_authToken=${WISED_PRIVATE_REPOSITORY_ACCESS_TOKEN}
+```
+
+> **_NOTE:_** your env must contain `WISED_PRIVATE_REPOSITORY_ACCESS_TOKEN` env var
+
+2. run `chmod +x generate-client.sh`
+3. run `./generate-client.sh`
---
+++ generate-client.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+AXIOS_PACKAGE_NAME=jira-cloud-client-ts-node
+mkdir $AXIOS_PACKAGE_NAME
+cd $AXIOS_PACKAGE_NAME
+npx @openapitools/openapi-generator-cli generate \
+-i https://developer.atlassian.com/cloud/jira/platform/swagger-v3.v3.json \
+--enable-post-process-file -g typescript-node -o . --additional-properties=npmName=@wised/$AXIOS_PACKAGE_NAME,npmRepository=https://wised.jfrog.io/artifactory/api/npm/wised-npm-repo/,paramNaming=original,enumPropertyNaming=original --skip-validate-spec
+
+npm install
+npm run build
+npm publish
