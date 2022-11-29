0d2658f6eb2d36f5e8eff144eb195f54058031c2
guy-wal <guyw@veriti.ai>
2022-11-29 14:24:00 +0200

    use axios. rename client generated Configuration class to ClientConfiguration

--- generate-client.sh
+++ generate-client.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-AXIOS_PACKAGE_NAME=jira-cloud-client-ts-node
+AXIOS_PACKAGE_NAME=jira-cloud-client-ts-axios
 mkdir $AXIOS_PACKAGE_NAME
 cd $AXIOS_PACKAGE_NAME
 npx @openapitools/openapi-generator-cli generate \
 -i https://developer.atlassian.com/cloud/jira/platform/swagger-v3.v3.json \
---enable-post-process-file -g typescript-node -o . --additional-properties=npmName=@wised/$AXIOS_PACKAGE_NAME,npmRepository=https://wised.jfrog.io/artifactory/api/npm/wised-npm-repo/,paramNaming=original,enumPropertyNaming=original --skip-validate-spec
+--enable-post-process-file -g typescript-axios -o . --additional-properties=npmName=@wised/$AXIOS_PACKAGE_NAME,npmRepository=https://wised.jfrog.io/artifactory/api/npm/wised-npm-repo/,paramNaming=original,enumPropertyNaming=original --skip-validate-spec
 
 npm install
 npm run build
